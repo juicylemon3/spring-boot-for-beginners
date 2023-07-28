c280f2c99199409dab2320d202ff143a7cc0cadc
EF-lance <jsbiblancea@itcom888.com>
2023-07-28 13:47:57 +0800

    init

---
+++ src/main/java/com/mananabas/springdemo/SpringDemoApplication.java
@@ -0,0 +1,16 @@
+package com.mananabas.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ef-lance
+ */
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mananabas/springdemo/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.mananabas.springdemo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author ef-lance
+ */
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping()
+    public List<String> findAllStudents(){
+
+        return List.of(
+               "Lorenz Igit",
+                "Hello World"
+        );
+    }
+
+}
